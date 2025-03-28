76a6eec2f3c307a2fc06a1f65b915f3097756274
JRueda-227 <133704470+JRueda-227@users.noreply.github.com>
2025-03-28 12:48:03 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+/*
+npm install --save-dev webpack webpack-cli html-webpack-plugin style-loader css-loader webpack-dev-server eslint-plugin-prettier eslint-config-prettier prettier --save-exact
+*/
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+    static: path.resolve(__dirname, "dist"),
+    open: true,
+    port: 8080,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
